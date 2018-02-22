997e68b08457e3885b72e0db686f9149e8f7bc90
Goomig <nathanielslater629@gmail.com>
2018-02-22 02:34:17 -0800

    Updated Config.js, DBManager.js, and TokensFormat.js

---
+++ src/database/DBManager.js
@@ -0,0 +1,162 @@
+/* eslint-disable */
+const { MongoClient } = require("mongodb");
+const { mongo } = require("../util/Tokens.js");
+
+class DefaultServer {
+  constructor(g) {
+    this.g_id = g.id
+    this.music = {
+      volume: 100,
+      queue: [],
+      song_banlist: {
+        enabled: false,
+        songs: []
+      }
+    };
+    this.moderation = {
+      spam_protect: false,
+      swear_filter: {
+        enabled: false,
+        words: []
+      },
+      mutes: [],
+      kicks: [],
+      bans: []
+    };
+    this.settings = {
+      notifications: false,
+      preferEmbeds: false,
+      noInvite: "",
+      noLink: "",
+      prefix: "default",
+      donated: false,
+      logs: {
+        member: "",
+        action: ""
+      },
+      auto_join_role: {
+        user: {
+          enabled: false,
+          role_id: ""
+        },
+        bot: {
+          enabled: false,
+          role_id: ""
+        }
+      },
+      user_leave_notify: false
+    };
+  }
+}
+
+class DefaultUser {
+  constructor(u) {
+    this.u_id = u.id;
+    this.afkReason = "";
+  }
+}
+
+class DBManager {
+  constructor(client) {
+    this.client = client;
+    this.url = `mongodb://${
+      mongo.username}:${
+      mongo.password}@${
+      mongo.host}:${
+      mongo.port}/${
+      mongo.database}`;
+    this.mcient = null,
+    this.db = null
+  }
+
+  // Debug
+  debug(...args) {
+    return this.client.debug(`[DB]`, ...args);
+  }
+  error(...args) {
+    return this.client.error('[DB]', ...args);
+  }
+  // Connect
+  async connect() {
+    this.debug('Creating Database...');
+    try {
+      this.mcient = await MongoClient.connect(this.url, null);
+    } catch (e) {
+      return this.error(e);
+    }
+    this.db = this.mcient.db(mongo.database);
+
+    this.db.on("close", mongoError => {
+      this.error("Database Randomly Closed");
+      if (mongoError) this.error(`Error: ${mongoError.stack}`);
+    });
+    this.db.on('error', mongoError => {
+      this.error('Database Internal Error');
+      if (mongoError) this.error(`Error: ${mongoError.stack}`);
+    });
+    this.db.on('reconnect', mongoError => {
+      this.debug('Database Reconnected');
+      if (mongoError) this.error(`Error: ${mongoError.stack}`);
+    });
+    this.db.on('timeout', mongoError => {
+      this.error('Database Timeout');
+      if (mongoError) this.error(`Error: ${mongoError.stack}`);
+    });
+
+    this.debug('Database Created.');
+    return this.db;
+  }
+
+  // Utilities
+  async makeNewGuild(g, isMissing = false) {
+    if (!this.db) throw new Error('Database Not Ready');
+    const guild = this.client.guilds.get(g);
+    if (!guild) throw new Error('Client Cannot Find Guild');
+    const gCollection = this.db.collection(this.client.config.mongodb.collections.servers);
+    if (!gCollection) throw new Error('Guild Database Missing');
+    await gCollection.deleteMany({ g });
+    const gData = new DefaultServer(g);
+    await gCollection.insertOne(gData);
+    this.debug(!guild ? `I've joined ${g}.` : `I've ${(isMissing ? 'added missing guild' : 'joined')} ` +
+      `${guild.name} (${guild.id}) owned by ${guild.owner.user.username} (${guild.owner.id}).`);
+    return gData;
+  }
+  
+  async makeNewUser(u) {
+    if (!this.db) throw new Error("Database Not Ready");
+    const user = this.client.users.get(u);
+    if (!user) throw new Error("Client Cannot Find User");
+    const uCollection = this.db.collection(this.client.config.mongodb.collections.users);
+    if (!uCollection) throw new Error('User Database Missing');
+    await uCollection.deleteMany({ u });
+    const uData = new DefaultServer(u);
+    await uCollection.insertOne(uData);
+    return uData;
+  }
+
+  async fetchGuildData(g) {
+    if (!this.db) throw new Error('Database Not Ready');
+    const gCollection = await this.getCollection(this.client.config.mongodb.collections.servers);
+    if (!gCollection) throw new Error('Guild or Donations Collection Missing');
+    let gData = await gCollection.findOne({ g_id: g });
+    if (!this.client.config.IsBeta && !gData) gData = await this.makeNewGuild(g, true);
+    if (!gData) return null;
+    delete gData._id;
+    return gData;
+  }
+
+  async fetchUserData(u) {
+    if (!this.db) throw new Error('Database Not Ready');
+    const uCollection = await this.getCollection(this.client.config.mongodb.collections.users);
+    if (!uCollection ) throw new Error('User Collection Missing');
+    let uData = await uCollection.findOne({ u_id: u });
+    if (!uData) return null;
+    return uData;
+  }
+
+  collection(name) {
+    return this.db.collection(name);
+  }
+}
+
+module.exports.DBManager = DBManager;
---
+++ src/util/Config.js
@@ -0,0 +1,54 @@
+/* eslint-disable max-len */
+module.exports = {
+  prefix: "e$",
+  dPrefix: "e#",
+  files: {
+    commands: "./commands",
+    events: "./events"
+  },
+  mdb: {
+    blacklist: "blacklist",
+    // donations: 'donations',
+    guilds: "guilds",
+    afk: "afk"
+  },
+  lang: {
+    underMaintenance: "Oh no, my code's being updated, please check back later! (Sorry but Echo is under maintenance right now)",
+    helpMsgs: [
+      /* Goomig */
+      "Help is flying through the sky, into your DMs!",
+      "Help is sliding on ice, into your DMs!",
+      /* Dwigoric */
+      "Sending 911 under your doormat!",
+      "Delivering the savory pizza in front of your door!",
+      "Sending angels into your mailbox!",
+      "Hooman, I have heeded your request for help.",
+      "So help me God",
+      "Helping you get out of your problems...",
+      "Magic stones, ye say? Right here under the kilt.",
+      "Helping you escape the shadow realm...",
+      "Let me check that I have this right...",
+      /* Goomig */
+      "Whoops, Slip, SQUASH THAT REQUEST FOR HELP HERE I COME!",
+      /* Dwigoric */
+      "I am really sorry to hear that. Is there anything I can do to do for your request for help?",
+      "That is a good question. Find it out yourself.",
+      "I'm not sure, but don't ask me for help.",
+      "I'm sorry, I don't have the information on that. May I put you on hold for a few minutes? I will clarify this with our manager.",
+      "I'm sorry, this question would be out of my expertise, but Daniel from the tech support department will be able to help you. Would you like me to connect you with him?",
+      "May I put your call on hold while I am checking your order?",
+      "I'm sorry, Echo is not available right now. May I help you with something?",
+      "I'm sorry, we don't have this feature at the moment. We do intend to add it to our service and we can notify you when it has been done. Would you like to receive an email update?",
+      /* Wistful */
+      "YA NEED HELP?",
+      /* Fiery_Hacker */
+      "Bringing in the reinforcements..."
+    ]
+  },
+  colors: {
+    orange: "#F4862C",
+    red: "#F2524A",
+    green: "#92EE8F"
+  },
+  owners: ["147891648628654082"]
+};
