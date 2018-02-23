caa85f146e5d0d1622bc85402422a482c5da2e69
Goomig <nathanielslater629@gmail.com>
2018-02-22 21:26:58 -0800

    Added Use of DevToken

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const { ShardingManager } = require("discord.js");
-const { token } = require("./util/Tokens.js");
-const sm = new ShardingManager("./client/Echo.js", { token });
+const { token, devToken } = require("./util/Tokens.js");
+const { maintenance } = require("./util/Config.js");
+const tokenToUse = maintenance ? devToken : token;
+const sm = new ShardingManager("./client/Echo.js", { tokenToUse });
 sm.spawn();
