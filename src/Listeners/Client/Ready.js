8568130652578e2c463b3c14a5021560f9a61129
Goomig <nathanielslater629@gmail.com>
2018-03-29 18:14:07 -0700

    Initial Commit

--- src/Listeners/Client/Ready.js
+++ src/Listeners/Client/Ready.js
@@ -1,18 +1,18 @@
-const { Listener } = require('discord-akairo');
+const { Listener } = require("discord-akairo");
 
 class ClientReady extends Listener {
   constructor() {
-    super('clientReady', {
-      emitter: 'client',
-      event: 'ready'
+    super("clientReady", {
+      emitter: "client",
+      event: "ready"
     });
   }
 
   exec() {
     /*
     this.client.user.setActivity(`${this.client.akairoOptions.prefix}help`, {
-      url: 'https://www.twitch.tv/monstercat/',
-      type: 'STREAMING'
+      url: "https://www.twitch.tv/monstercat/",
+      type: "STREAMING"
     });
     */
     console.log(`Online and ready! This shard is on ${this.client.guilds.size} guilds.`);
