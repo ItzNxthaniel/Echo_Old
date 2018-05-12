787018d89d6560cdb5f94b0dc008f0e90ae3c660
Goomig <nathanielslater629@gmail.com>
2018-05-11 23:29:08 -0700

    Updated Restart Command

--- src/Commands/Owner/Restart.js
+++ src/Commands/Owner/Restart.js
@@ -17,12 +17,9 @@ class Restart extends Command {
     if (args.content == "all") {
       await m.channel.send("Restarting, the whole bot!");
       return process.exit();
-    } else if (args.content != "all") {
-      await m.channel.send(`Restarting shard ${this.client.shard.id}.`);
-      return process.exit();
     } else {
-      await m.channel.send(`Attempting to restart shard ${args.shardID}.`);
-      await this.client.shard.broadcastEval(`if (this.shard.id == ${args.shardID.toString()}) process.exit();`);
+      await m.channel.send(`Attempting to restart shard ${args.content}.`);
+      await this.client.shard.broadcastEval(`if (this.shard.id == ${args.content.toString()}) process.exit();`);
     }
   }
 }
