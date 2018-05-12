dc14b97173a345e2d07bd7755e94726a0b9aab2e
Goomig <nathanielslater629@gmail.com>
2018-05-11 23:33:14 -0700

    Attempt to fix the break

--- src/Listeners/Client/Ready.js
+++ src/Listeners/Client/Ready.js
@@ -9,13 +9,6 @@ class ClientReady extends Listener {
   }
 
   exec() {
-    /*
-    this.client.user.setActivity(`${this.client.akairoOptions.prefix}help`, {
-      url: "https://www.twitch.tv/monstercat/",
-      type: "STREAMING"
-    });
-    */
-
     this.client.user.setActivity(`on ${this.client.guilds.size} ${this.client.guilds.size == 1 ? "server!" : "servers!"}`);
     console.log(`Online and ready! This shard is on ${this.client.guilds.size} guilds.`);
   }
