6e4d61b60ab0b65d0f7e72224281d444f8547c89
Goomig <nathanielslater629@gmail.com>
2018-09-30 01:55:18 -0700

    Updated Ping Once Again

--- src/Commands/Example/Ping.js
+++ src/Commands/Example/Ping.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
   async exec(m) {
     m.channel.send("Pinging!").then(snt => {
       snt.edit("", { embed: {
-	      color: 0x92ee8f, title: "Ping",
+		  color: 0x92ee8f, title: "Ping",
           fields: [
 		    { name: "Bot Ping:", value: `${Math.floor(Math.round(bot.ping))}ms`, inline: true },
 		    { name: "Message Ping", value: `${Math.floor(Math.round(snt.createdTimestamp - m.createdTimestamp))}ms`, inline: true }
