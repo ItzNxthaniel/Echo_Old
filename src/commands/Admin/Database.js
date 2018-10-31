2d783eb7afb16a9f22d97d800dbb58d5ff13f09a
Dwigoric <dwigoric@gmail.com>
2018-10-31 10:34:54 +0800

    Fix Database.js

--- src/commands/Admin/Database.js
+++ src/commands/Admin/Database.js
@@ -1,23 +1,23 @@
 const { Command, KUtil } = require('../../Modules/Index.js');
+const { inspect } = require('util');
 
 module.exports = class extends Command {
 
   constructor(...args) {
     super(...args, {
       description: 'The base command for getting database information',
-      usage: '<debug|reset|help> [GuildID:string]',
-      usageDelim: ' ',
+      usage: '<debug|reset>',
       subcommands: true
-    })
+    });
   }
 
   async debug(m) {
-    m.send(KUtil.codeBlock(m.language, await this.client.providers.default.get('guilds', m.guild.id)));
-  };
+    m.send(KUtil.codeBlock('js', inspect(await this.client.providers.default.get('guilds', m.guild.id))));
+  }
 
   async reset(m) {
     this.client.guilds.get(m.guild.id).settings.reset();
-    m.send(`Guild **${guildID}**'s database has been reset.`);
-  };
+    m.send(`Guild **${m.guild.id}**'s database has been reset.`);
+  }
 
 };
