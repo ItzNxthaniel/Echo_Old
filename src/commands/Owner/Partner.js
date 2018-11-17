b2561ca4848d520347c0a29ce2ed98be32ffdb8e
Dwigoric <dwigoric@gmail.com>
2018-11-17 12:01:48 +0800

    Return null instead of undefined

--- src/commands/Owner/Partner.js
+++ src/commands/Owner/Partner.js
@@ -24,7 +24,7 @@ module.exports = class extends Command {
     if (m.channel.id !== "451241090301820948" && m.channel.id !== "451240344953028608") return m.channel.send("This command can only be ran in the #partner-commands channel.");
     if (!m.guild.members.get(oid)) return m.channel.send("That user is not in this server!");
     if (!this.client.guilds.get(id)) return m.channel.send("That server has not added Echo!");
-    return;
+    return null;
   }
 
   async remove(m, [id]) {
